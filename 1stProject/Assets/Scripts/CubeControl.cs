6727a0f0edbb2c9cd48de625af1af108f0d3a9fa
하수영 <134994862+GitJinseong@users.noreply.github.com>
2023-07-04 11:54:07 +0900

    Init script files

---
+++ 1stProject/Assets/Scripts/CubeControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+    public float rotationSpeed = 0f;
+    private Vector3 cubeRotation = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("이게 Unity의 Hello World다.");
+        transform.Rotate(new Vector3(0f, 45f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("이것도 어디선가 Call 하고 있다.");
+
+        cubeRotation.y += rotationSpeed;
+        transform.Rotate(cubeRotation);
+    }
+}
